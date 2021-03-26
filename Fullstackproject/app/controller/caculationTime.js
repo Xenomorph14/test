86e0611dbfbfd620330a51bba814063818b0edcc
nhatban25912345 <hoangnhat529@gmail.com>
2021-03-26 19:09:45 +0700

    update caculationTime

--- Fullstackproject/app/controller/caculationTime.js
+++ Fullstackproject/app/controller/caculationTime.js
@@ -14,6 +14,8 @@ module.exports = (req,res) => {
         let seconds = Math.floor(caculationTime/1000);
         let minutes = Math.floor(seconds/60);
         let hours = Math.floor(minutes/60);
+        minutes = minutes - hours*60;
+        seconds = seconds - hours*60*60 - minutes*60;
         Status.findByIdAndUpdate(
             id,
             {
@@ -25,7 +27,7 @@ module.exports = (req,res) => {
                     console.log("update failed!");
                     return res.redirect("/createMember")
                 }
-                console.log(status);
+                console.log(status.timeKeeping);
                 console.log("Update time keeping success!");
                 res.redirect("/createMember")
             }
